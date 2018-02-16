646adea77ceb9797c14ba7d0b2f3b9e1d92cfb24
WINDOWS-PC\Prueba <jhoel9211@gmail.com>
2018-02-16 00:23:21 -0500

    primer commit

---
+++ CallCenterService/src/main/java/Jhoel/CallCenterService/App.java
@@ -0,0 +1,17 @@
+package Jhoel.CallCenterService;
+
+/**
+ * 
+ * @author Prueba
+ *
+ */
+public class App {
+	
+	/**
+	 * Hello world!
+	 *
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
